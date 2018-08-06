25317a422150840904be133c480849179292dc4a
JasonWilltrue <148373644@qq.com>
2018-08-06 23:09:19 +0800

    routejs中加入switch增加路由判断

--- src/pages/ui/buttons.js
+++ src/pages/ui/buttons.js
@@ -1,26 +1,72 @@
-import React,{Component} from 'react';
-import {Card,Button} from 'antd';
-export default class Buttons extends Component{
+import React, { Component } from 'react';
+import { Card, Button,Radio } from 'antd';
 
-     render(){
-         return(
+
+const ButtonGroup = Button.Group;
+export default class Buttons extends Component {
+
+    state = {
+        loadding: true,
+        size:'default',
+    }
+
+    handleLoading = () => {
+        this.setState({
+            loadding: false
+        })
+    }
+
+    onchangSize=(e)=>{
+       this.setState({
+        size:e.target.value,
+       })
+    }
+
+
+    render() {
+
+        return (
             <div>
-                 <Card title="基础按钮">
-                     <Button type="primary">Imooc</Button>
-                     <Button type="">Imooc</Button>
-                     <Button type="dashed">Imooc</Button>
-                     <Button type="danger">Imooc</Button>
-                     <Button  disabled>Imooc</Button>
-                 </Card>
-                 <Card title="图像按钮">
-                     <Button type="primary">Imooc</Button>
-                     <Button type="">Imooc</Button>
-                     <Button type="dashed">Imooc</Button>
-                     <Button type="danger">Imooc</Button>
-                     <Button  disabled>Imooc</Button>
-                 </Card>
-             </div>
-         )
-     }
+                <Card title="基础按钮">
+                    <Button type="primary">Imooc</Button>
+                    <Button type="">Imooc</Button>
+                    <Button type="dashed">Imooc</Button>
+                    <Button type="danger">Imooc</Button>
+                    <Button disabled>Imooc</Button>
+                </Card>
+                <Card title="图像按钮">
+                    <Button icon="plus">创建</Button>
+                    <Button icon="edit">编辑</Button>
+                    <Button type="delete">删除</Button>
+                    <Button shape="circle" icon="search"></Button>
+                    <Button type="primary" icon="search">搜索</Button>
+                    <Button type="primary" icon="download">下载</Button>
+                </Card>
+                <Card title="加载按钮">
+                    <Button type="primary" loading={this.state.loadding}>确定</Button>
+                    <Button type="primary" shape="circle" loading={this.state.loadding}></Button>
+                    <Button loading={this.state.loadding}>点击加载</Button>
+                    <Button shape="circle" loading={this.state.loadding}></Button>
+                    <Button type="primary" icon="close" onClick={this.handleLoading}>关闭</Button>
+                </Card>
+                <Card title="按钮组">
+                    <ButtonGroup>
+                        <Button type="primary" icon="left">返回</Button>
+                        <Button type="primary" icon="right">前进</Button>
+                    </ButtonGroup>
+                </Card>
+                <Card title="按钮尺寸">
+                     <Radio.Group onChange={this.onchangSize} vlaue={this.state.size}>
+                        <Radio value="small">小</Radio>
+                        <Radio value="default">中</Radio>
+                        <Radio value="large">大</Radio>
+                     </Radio.Group>
+                    <Button type="primary" size={this.state.size}>imooc</Button>
+                    <Button type="primary" size={this.state.size}>imooc</Button>
+                    <Button type="primary" size={this.state.size}>imooc</Button>
+                </Card>
+            </div>
+        )
+    }
 
 }
--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { HashRouter, Route } from 'react-router-dom';
+import { HashRouter, Route,Switch } from 'react-router-dom';
 import App from './App';
 import Login from './pages/login';
 import Admin from './admin';
 import Button from './pages/ui/buttons';
 import NoMatch from './pages/ui/noMatch';
+
 
 
 export default class IRouter extends Component {
@@ -16,8 +17,10 @@ export default class IRouter extends Component {
                         <Route path="/login" component={Login} />
                         <Route path="/admin" render={() =>
                             <Admin>
+                                <Switch>
                                 <Route path="/admin/ui/buttons" component={Button} />
                                 <Route  component={NoMatch} />
+                                </Switch>
                             </Admin>
                         }/>
                     </App>
