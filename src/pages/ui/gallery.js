4f44c5bb88ff545423ba6b72dd64193b06ff14d7
JasonWilltrue <148373644@qq.com>
2018-08-10 00:21:37 +0800

    增加画廊

--- src/pages/ui/gallery.js
+++ src/pages/ui/gallery.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react'
-import { Card,  Row,Col} from 'antd';
+import { Card,  Row,Col,Modal} from 'antd';
 import './ui.less';
 
 export default class Gallery extends Component {
+    state = {
+        visible :false,
+        imgSrc:'',
+    }
+    openGallery = (item)=>{
+       this.setState({
+           visible:true,
+           imgSrc:'./gallery/'+item
+       })
+    }
     render() {
         const imgs = [
             ['1.png','2.png','3.png','4.png','5.png'],
@@ -12,31 +22,47 @@ export default class Gallery extends Component {
             ['1.png','2.png','3.png','4.png','5.png']
         ]
         const imgList = imgs.map((list)=>list.map((item)=>
-            <Card cover={<img src={'./gallery/'+item} alt="一张图片" />}>
+            <Card
+            style={{marginBottom:10}}
+            cover={<img src={'./gallery/'+item} alt="一张图片" onClick={()=>{this.openGallery(item)}} />}>
                 <Card.Meta
                  title="这是一张照片"
                  description="这是一段描述！！！"
                 />
              </Card>
         ))
-        return (<Card title="全局提示框" className="card-warp">
-                     <Row>
-                        <Col md={5}>
-                        {imgList[0]}
-                        </Col>
-                        <Col md={5}>
-                        {imgList[1]}
-                        </Col>
-                        <Col md={5}>
-                        {imgList[2]}
-                        </Col>
-                        <Col md={5}>
-                        {imgList[3]}
-                        </Col>
-                        <Col md={4}>
-                        {imgList[4]}
-                        </Col>
-                     </Row>
-                </Card>)
+        return ([<Card title="全局提示框" className="card-warp">
+        <Row gutter={10}>
+           <Col md={5}>
+           {imgList[0]}
+           </Col>
+           <Col md={5}>
+           {imgList[1]}
+           </Col>
+           <Col md={5}>
+           {imgList[2]}
+           </Col>
+           <Col md={5}>
+           {imgList[3]}
+           </Col>
+           <Col md={4}>
+           {imgList[4]}
+           </Col>
+        </Row>
+   </Card>,
+   <Modal
+   title="图片预览"
+   width={300}
+   height={500}
+   visible={this.state.visible}
+   onCancel = {()=>{this.setState({
+       visible:false
+   })}}
+   footer = {null}
+   >
+    <img src={this.state.imgSrc} alt="" style={{width:'100%'}}/>
+   </Modal>]
+
+            )
     }
 }
