6d3a397475ecbb707a59cbe254d0dd7a8f67ce74
JasonWilltrue <148373644@qq.com>
2018-08-11 23:47:56 +0800

    动态获取表单

---
+++ src/axios/index.js
@@ -0,0 +1,38 @@
+/**
+ *  怎么样封装promise    出错处理？？？
+ */
+import axios from "axios";
+import { Modal } from "antd";
+export default class Axios {
+  static ajax(options) {
+    let baseUrl =
+      "https://www.easy-mock.com/mock/59b09f37e0dc6633419fb9eb/antdbike";
+    return new Promise((resolve, rejects) => {
+      axios({
+        url: options.url,
+        method: "get",
+        baseURL: baseUrl,
+        timeout: 5000,
+        params: (options.data && options.data.params) || ""
+        //如果跨域 还需要带header  cookie
+      }).then(responese => {
+        //表示请求成功而已，数据不一定正确需要进一步判断
+        if (responese.status === "200") {
+          let res = responese.data;
+          //业务逻辑代码返回0
+          if (res.code === 0) {
+            resolve(res.result);
+          } else {
+            Modal.info({
+              title: "提示",
+              content: res.message
+            });
+          }
+        } else {
+          //报错了，拦截错误消息
+          rejects(responese.data);
+        }
+      });
+    });
+  }
+}
--- src/pages/table/basictable.js
+++ src/pages/table/basictable.js
@@ -1,76 +1,116 @@
-import React, { Component } from 'react'
-import { Card, Table} from 'antd';
-
+import React, { Component, Fragment } from "react";
+import { Card, Table } from "antd";
+import axios from "./../../axios";
 
 export default class BasicTable extends Component {
-    state={
-        dataSource:[]
-    }
+  state = {
+    dataSource: [],
+    dataSource2: []
+  };
 
-    componentDidMount(){
-        //类似与表头文件
-        const dataSource = [
-            {
-                id:'1',
-                name:'jack',
-                password:'123456',
-                datetime:'2018-08-11'
-            },
-            {
-                id:'2',
-                name:'jack',
-                password:'123456',
-                datetime:'2018-08-11'
-            },
-            {
-                id:'3',
-                name:'jack',
-                password:'123456',
-                datetime:'2018-08-11'
-            },
-            {
-                id:'4',
-                name:'jack',
-                password:'123456',
-                datetime:'2018-08-11'
-            }
-        ]
-        this.setState({
-            dataSource
-        })
-    }
+  componentDidMount() {
+    //类似与表头文件
+    const dataSource = [
+      {
+        id: "1",
+        name: "jack",
+        password: "123456",
+        datetime: "2018-08-11"
+      },
+      {
+        id: "2",
+        name: "jack",
+        password: "123456",
+        datetime: "2018-08-11"
+      },
+      {
+        id: "3",
+        name: "jack",
+        password: "123456",
+        datetime: "2018-08-11"
+      },
+      {
+        id: "4",
+        name: "jack",
+        password: "123456",
+        datetime: "2018-08-11"
+      }
+    ];
+    this.setState({
+      dataSource
+    });
+    this.request();
+  }
+  /**
+   * 动态获取默认表格数据
+   */
+  // request = ()=>{
+  //    axios.get('https://www.easy-mock.com/mock/59b09f37e0dc6633419fb9eb/antdbike/table/list1')
+  //    .then(res =>{
+  //      console.log(JSON.stringify(res));
+  //      if(res.data.result && res.data.code === 0){
+  //         this.setState({
+  //           dataSource2:res.data.result.list
+  //         })
+  //      }
+  //    })
+  // }
+  //===================封装后的新写法====================
+  request = () => {
+    axios.ajax({
+      url: "/table/list1",
+      data: {
+        params: {
+          page: 1
+        }
+      }
+    }).then(res=>{
+      if(res.code === 0){
+         this.setState({
+          dataSource2:res.list
+         })
+      }
+    });
+  };
 
-
-    render() {
-        const columns = [
-            {
-                title:'id',
-                dataIndex:'id'
-            },
-            {
-                title:'用户名',
-                dataIndex:'name'
-            },
-            {
-                title:'密码',
-                dataIndex:'password'
-            },
-            {
-                title:'注册时间',
-                dataIndex:'datetime'
-            }
-        ]
-        return (
-            <Card title="基础表格" className="card-warp">
-               <Table
-                bordered
-                columns={columns}
-                dataSource = {this.state.dataSource}
-                pagination={false}
-               >
-               </Table>
-           </Card>
-        )
-
-    }
+  render() {
+    const columns = [
+      {
+        title: "id",
+        dataIndex: "id"
+      },
+      {
+        title: "用户名",
+        dataIndex: "name"
+      },
+      {
+        title: "密码",
+        dataIndex: "password"
+      },
+      {
+        title: "注册时间",
+        dataIndex: "datetime"
+      }
+    ];
+    return (
+      <Fragment>
+        <Card title="基础表格" className="card-warp">
+          <Table
+            bordered
+            columns={columns}
+            dataSource={this.state.dataSource}
+            pagination={false}
+          />
+        </Card>
+        <Card title="动态获取表格数据" className="card-warp">
+          <Table
+            bordered
+            columns={columns}
+            dataSource={this.state.dataSource2}
+            pagination={false}
+          />
+        </Card>
+      </Fragment>
+    );
+  }
 }
