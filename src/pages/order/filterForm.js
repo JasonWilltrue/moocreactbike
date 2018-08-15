c25bdc634bad2ca1180bfd175b75215c0e36e8bc
JasonWilltrue <148373644@qq.com>
2018-08-16 06:51:18 +0800

    取消订单事件

--- src/pages/order/filterForm.js
+++ src/pages/order/filterForm.js
@@ -1,61 +1,68 @@
 import React, { Component } from "react";
-import {Button, Form, Select,DatePicker} from "antd";
-
+import { Button, Form, Select, DatePicker } from "antd";
 
 const Option = Select.Option;
 const FormItem = Form.Item;
 
-class FilterForm extends Component{
-     render(){
-         const {getFieldDecorator} = this.props.form;
-         return(
-             <Form layout="inline">
-                <FormItem label="城市">
-                   {
-                       getFieldDecorator('city_id')(
-                             <Select placeholder="全部"
-                              style={{width:80}}
-                             >
-                             <Option value="">全部</Option>
-                            <Option value="1">北京</Option>
-                            <Option value="2">上海</Option>
-                            <Option value="3">杭州</Option>
-                           </Select>
-                       )
-                   }
-                </FormItem>
-                <FormItem label="订单时间">
-                   {
-                       getFieldDecorator('start_time')(
-                           <DatePicker showTime format="YYYY-MM-DD HH:mm:ss"/>
-                       )
-                   }
-                    {
-                       getFieldDecorator('end_time')(
-                           <DatePicker showTime format="YYYY-MM-DD HH:mm:ss"/>
-                       )
-                   }
-                </FormItem>
-                <FormItem label="订单状态">
-                   {
-                       getFieldDecorator('op_mode')(
-                           <Select placeholder="全部" style={{width:120}}>
-                            <Option value="">全部</Option>
-                            <Option value="0">进行中</Option>
-                            <Option value="1">进行中（暂时）</Option>
-                            <Option value="2">结束行程</Option>
-                           </Select>
-                       )
-                   }
-                </FormItem>
+class FilterForm extends Component {
 
-                <FormItem >
-                   <Button icon="search" type="primary" style={{margin:"0 20px"}}>查询</Button>
-                   <Button icon="reload" type="default">重置</Button>
-                </FormItem>
-             </Form>
-         )
-     }
+  handleSearch = () => {
+       let searchInfo = this.props.form.getFieldsValue();
+       console.log(searchInfo);
+  };
+  handleReload =()=>{
+
+  }
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form layout="inline">
+        <FormItem label="城市">
+          {getFieldDecorator("city_id",{
+          })(
+            <Select placeholder="全部" style={{ width: 80 }}>
+              <Option value="">全部</Option>
+              <Option value="1">北京</Option>
+              <Option value="2">上海</Option>
+              <Option value="3">杭州</Option>
+            </Select>
+          )}
+        </FormItem>
+        <FormItem label="订单时间">
+          {getFieldDecorator("start_time")(
+            <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+          )}
+          {getFieldDecorator("end_time")(
+            <DatePicker showTime format="YYYY-MM-DD HH:mm:ss" />
+          )}
+        </FormItem>
+        <FormItem label="订单状态">
+          {getFieldDecorator("op_mode")(
+            <Select placeholder="全部" style={{ width: 120 }}>
+              <Option value="">全部</Option>
+              <Option value="0">进行中</Option>
+              <Option value="1">进行中（暂时）</Option>
+              <Option value="2">结束行程</Option>
+            </Select>
+          )}
+        </FormItem>
+
+        <FormItem>
+          <Button
+            icon="search"
+            type="primary"
+            style={{ margin: "0 20px" }}
+            onClick={this.handleSearch}
+          >
+            查询
+          </Button>
+          <Button icon="reload" type="default" onClick={this.handleReload}>
+            重置
+          </Button>
+        </FormItem>
+      </Form>
+    );
+  }
 }
 //用于表单的双向绑定
 export default Form.create({})(FilterForm);
--- src/pages/order/index.js
+++ src/pages/order/index.js
@@ -1,25 +1,27 @@
-import  React, { Component,Fragment } from 'react';
-import { Form, Select, Card,Button, Table} from "antd";
+import React, { Component, Fragment } from "react";
+import { Card, Button, Table,Modal } from "antd";
 import axios from "./../../axios";
 import Utils from "../../utils/utils";
-import FilterForm from './filterForm';
-
+import FilterForm from "./filterForm";
 
 export default class Order extends Component {
-    state={
-      list:[],
-      pagination:null,
-    }
- //当前页
- params = {
-  page: 1
-};
+  state = {
+    list: [],
+    pagination: null,
+    selectedRowKeys: null,
+    selectedItem:[],
+    orderConfirmVisble:false
+  };
+  //当前页
+  params = {
+    page: 1
+  };
 
-    componentDidMount(){
-       this.request()
-    }
+  componentDidMount() {
+    this.request();
+  }
 
-   request = ()=>{
+  request = () => {
     const _this = this;
     axios
       .ajax({
@@ -30,27 +32,46 @@ export default class Order extends Component {
           },
           isShowLoading: true
         }
-      }).then((res)=>{
-        if(res.code === 0)
-        {
-            console.log(res.result.item_list);
-            res.result.item_list.map((item,index)=>{
-                   item.key = index
-            })
-            this.setState({
-               list:res.result.item_list,
-               pagination:Utils.pagination(res,(current)=>{
-                    _this.params.page = current;
-                    _this.request()
-               })
-            })
-        }
       })
-   }
+      .then(res => {
+        if (res.code === 0) {
+          console.log(res.result.item_list);
+          res.result.item_list.map((item, index) => {
+            item.key = index;
+          });
+          this.setState({
+            list: res.result.item_list,
+            pagination: Utils.pagination(res, current => {
+              _this.params.page = current;
+              _this.request();
+            })
+          });
+        }
+      });
+  };
+  onRowClick = (record, index) => {
+    console.log(record, index);
+    let selectKey = [index];
+    this.setState({
+      selectedRowKeys: selectKey,
+      selectedItem: record
+    });
+  };
+  handleConfirm =()=>{
+     this.setState({
+       orderConfirmVisble:true
+     })
+  }
+  handleOrderFinsh = ()=>{
 
-
-
+  }
   render() {
+    //定义一个数组 用于onchange切换使用
+    const { selectedRowKeys } = this.state;
+    const rowSelection = {
+      type: "radio",
+      selectedRowKeys
+    };
     const columns = [
       {
         title: "订单编号",
@@ -68,62 +89,54 @@ export default class Order extends Component {
         title: "用户名",
         key: "user_name",
         width: 80,
-        dataIndex: "user_name",
-
+        dataIndex: "user_name"
       },
       {
         title: "手机号",
         key: "mobile",
         width: 80,
-        dataIndex: "mobile",
-
+        dataIndex: "mobile"
       },
       {
         title: "里程",
         key: "distance",
         width: 80,
         dataIndex: "distance",
-        render:(distance)=>{
-           return `${distance}公里`
+        render: distance => {
+          return `${distance}公里`;
         }
-
       },
       {
         title: "行程时间",
         key: "total_time",
         width: 80,
-        dataIndex: "total_time",
-
-
+        dataIndex: "total_time"
       },
       {
         title: "状态",
         key: "status",
         width: 80,
         dataIndex: "status",
-        render:(status)=>{
+        render: status => {
           let config = {
-            "1":'进行中',
-            "2":'进行中(暂时)',
-            "3":'结束',
-          }
-          return config[status]
+            "1": "进行中",
+            "2": "进行中(暂时)",
+            "3": "结束"
+          };
+          return config[status];
         }
-
       },
       {
         title: "开始时间",
         key: "start_time",
         width: 120,
-        dataIndex: "start_time",
-
+        dataIndex: "start_time"
       },
       {
         title: "结束时间",
         key: "end_time",
         width: 120,
-        dataIndex: "end_time",
-
+        dataIndex: "end_time"
       },
       {
         title: "订单金额",
@@ -137,26 +150,49 @@ export default class Order extends Component {
         key: "user_pay",
         dataIndex: "user_pay"
       }
-
     ];
     return (
       <Fragment>
-          <Card>
-             <FilterForm />
-         </Card>
-         <Card style={{marginTop:10}}>
-             <Button icon="profile" type="primary" >订单详情</Button>
-             <Button icon="close-circle-o" type="primary" >取消订单</Button>
-         </Card>
-         <div className="content-warp">
-             <Table
-               bordered
-               columns={columns}
-               dataSource = {this.state.list}
-               pagination = {this.state.pagination}
-             />
-         </div>
+        <Card>
+          <FilterForm />
+        </Card>
+        <Card style={{ marginTop: 10 }}>
+          <Button icon="profile" type="primary">
+            订单详情
+          </Button>
+          <Button icon="close-circle-o" type="primary" onClick={this.handleConfirm}>
+            取消订单
+          </Button>
+        </Card>
+        <div className="content-warp">
+          <Table
+            bordered
+            rowSelection={rowSelection} //单选多选  类型配置
+            columns={columns}
+            dataSource={this.state.list}
+            pagination={this.state.pagination}
+            onRow={(record, index) => {
+              return {
+                onClick: () => {
+                  this.onRowClick(record, index);
+                }, //点击行
+                onMouseEnter: () => {} //鼠标移入行
+              };
+            }}
+          />
+        </div>
+        <Modal
+           title="取消订单"
+           visible = {this.state.orderConfirmVisble}
+           onCancel={()=>{
+             this.setState({
+                orderConfirmVisble:false
+             })
+           }}
+           onOk={this.handleOrderFinsh}
+        >
+        </Modal>
       </Fragment>
-    )
+    );
   }
 }
