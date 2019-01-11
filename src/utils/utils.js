ca3790cc5f4d4e5d336d0fa88bfed6a99e464c83
lovewcc <148373644@qq.com>
2019-01-11 22:57:22 +0800

    修改promise封装函数

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -2,59 +2,77 @@
  * @pagination 分页功能
  */
 import React from 'react';
-import { Select } from "antd";
+import {
+    Select
+} from "antd";
 const Option = Select.Option;
 export default {
-    formateDate(time){
-       if(!time) return "";
-       let date = new Date(time);
-       return date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDay();
+    /**
+     * 获取当前时间
+     * @param {时间} time 
+     */
+    formateDate(time) {
+        if (!time) return "";
+        let date = new Date(time);
+        return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDay();
     },
-     pagination(data,callback){
-       let page = {
-           onChange:(current)=>{
-               callback(current); //告诉他们我们要干嘛
-           },
-           current : data.result.page,  //读取请求分页的数据  当前页数
-           pageSize:data.result.page_size,    //每页条数
-           total:data.result.total_count,        //总条数
-           showTotal:()=>{
+    /**
+     * 分页显示
+     * @param {*} data 
+     * @param {*} callback 
+     */
+    pagination(data, callback) {
+        let page = {
+            onChange: (current) => {
+                callback(current); //告诉他们我们要干嘛
+            },
+            current  : data.result.page,          //读取请求分页的数据  当前页数
+            pageSize : data.result.page_size,     //每页条数
+            total    : data.result.total_count,   //总条数
+            showTotal: () => {
                 return `总共${data.result.total_count}条数据，当前第${data.result.page}页`
-           },
-           showQuickJumper:false,//是否开启分页跳转
-       }
-       return page;
+            },
+            showQuickJumper: false,   //是否开启分页跳转
+        }
+        return page;
 
-     },
-     getOptionList(data){
-         if(!data){
-             return [];
-         }
+    },
+    getOptionList(data) {
+        if (!data) {
+            return [];
+        }
         //  let options = [<Option value="0" key="all_key"></Option>];
         let options = [];
-         data.map((item) =>{
-             options.push(<Option value={item.id} key={item.id}>{item.name}</Option>)
-         })
-         return options;
-     },
+        data.map((item) => {
+            options.push(<Option value={
+                item.id
+            }
+                key={
+                    item.id
+                } > {
+                    item.name
+                } </Option>)
+        })
+        return options;
+    },
 
 
-     updateSelectedItem(selectKey,record,selectedIds){
-        if(selectedIds){
+    updateSelectedItem(selectKey, record, selectedIds) {
+        if (selectedIds) {
             this.setState({
                 selectedRowKeys: selectKey,
-                selectedItem: record,
-                selectedIds:selectedIds,
-              });
-        }else{
+                selectedItem   : record,
+                selectedIds    : selectedIds,
+            });
+        } else {
             this.setState({
                 selectedRowKeys: selectKey,
-                selectedItem: record
-              });
+                selectedItem   : record
+            });
         }
 
-          console.log(record,selectedIds);
+        console.log(record, selectedIds);
 
-     }
+    }
 
 }
