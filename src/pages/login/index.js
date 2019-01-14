a30bbe687dfcd147d89da3a02af204995420703f
lovewcc <148373644@qq.com>
2019-01-14 23:17:55 +0800

    设置登录界面

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,12 +1,14 @@
-import React,{Component} from 'react';
-export default class Login extends Component{
+import React, { Component } from 'react';
+export default class Login extends Component {
 
-     render(){
-         return(
+    render() {
+        return (
             <div>
-                 this is Login page
-             </div>
-         )
-     }
+                <div>ELM后台</div>
+                <div>Bike后台</div>
+                <div>其他后台</div>
+            </div>
+        )
+    }
 
 }
