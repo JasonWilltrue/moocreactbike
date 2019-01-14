aaee7523e00870ad0645eb5101bbe2c23886e205
lovewcc <148373644@qq.com>
2019-01-14 23:20:50 +0800

    小调整

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -7,6 +7,17 @@ export default class Login extends Component {
                 <div>ELM后台</div>
                 <div>Bike后台</div>
                 <div>其他后台</div>
+                <div className="loginlayout">
+                    <div>
+                        <span>用户名：</span>
+                        <input type="text" placeholder="输入账号" />
+                    </div>
+                    <div>
+                        <span>密码：</span>
+                        <input type="password" placeholder="输入密码" />
+                    </div>
+                    <div>登录</div>
+                </div>
             </div>
         )
     }
